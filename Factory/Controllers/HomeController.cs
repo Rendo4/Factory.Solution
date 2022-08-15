1c551ff4aec784a4a96996c828589b827029cead
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2022-08-15 08:51:36 -0700

    set up splash page

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
+using System;
+
+namespace Factory.Controllers
+{
+    public class HomeController : Controller
+    {
+      private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        ViewBag.PageTitle ="Home";
+        ViewBag.Tab="home";
+        List<MachineEngineer> model = _db.MachineEngineer.ToList();
+        return View(model);
+      }
+
+    }
+}
